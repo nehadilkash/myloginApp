21d136cc0a107a2e196ffdaa41139ad189d83ec2
nehadilkash <nehadilkash@gmail.com>
2020-02-11 17:15:02 +0530

    Add files via upload
    it is sql script required o build database for the application.

---
+++ mylogin/sql script.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `user_roles` (
+  `users_id` bigint(20) NOT NULL,
+  `roles_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`users_id`,`roles_id`),
+  KEY `FKj9553ass9uctjrmh0gkqsmv0d` (`roles_id`),
+  CONSTRAINT `FK7ecyobaa59vxkxckg6t355l86` FOREIGN KEY (`users_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FKj9553ass9uctjrmh0gkqsmv0d` FOREIGN KEY (`roles_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
